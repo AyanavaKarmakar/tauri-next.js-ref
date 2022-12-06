e9fc11e87b8d86c0afda82b0576693fbb9b3c2d7
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-06 12:37:58 +0000

    chore: add comments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
+// disables the command prompt window that would normally pop up on Windows if you run a bundled app
 #![cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
 
+// The main function is the entry point
+// and the first function that gets invoked when your program runs.
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
